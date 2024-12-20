2be22ffcae092a965ba78a067a2c41c2aa5f66ca
Akhila N <akhilan0707@gmail.com>
2024-12-20 22:34:57 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Println("Welcome  to online Ticket booking", conferenceName, " Application")
+	fmt.Println("we have total of", conferenceTickets, "Tickets and ", remainingTickets, "still 50 are aviable")
+	fmt.Println("Get your tickets here")
+}
